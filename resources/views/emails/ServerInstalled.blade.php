39404c53b35dcaa225a5603445742a382c9fbd3e
Daniel Chu <danielchu096@gmail.com>
2024-06-28 16:34:35 +1200

    Create ServerInstalled.blade.php

---
+++ resources/views/emails/ServerInstalled.blade.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="x-apple-disable-message-reformatting">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="format-detection" content="telephone=no, date=no, address=no, email=no">
+    <style type="text/css">
+        body, table, td {font-family: Helvetica, Arial, sans-serif !important;}
+        .ExternalClass {width: 100%;}
+        .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {line-height: 150%;}
+        a {text-decoration: none;}
+        * {color: inherit;}
+        a[x-apple-data-detectors], u + #body a, #MessageViewBody a {color: inherit; text-decoration: none; font-size: inherit; font-family: inherit; font-weight: inherit; line-height: inherit;}
+        img { -ms-interpolation-mode: bicubic; }
+        table { border-spacing: 0px; border-collapse: collapse; }
+        table td { border-spacing: 0px; border-collapse: collapse; }
+        @media screen and (max-width: 600px) {
+            .w-full, .w-full > tbody > tr > td { width: 100% !important; }
+            .w-24, .w-24 > tbody > tr > td { width: 96px !important; }
+            .w-40, .w-40 > tbody > tr > td { width: 160px !important; }
+            .p-lg-10:not(table), .p-lg-10:not(.btn) > tbody > tr > td, .p-lg-10.btn td a { padding: 0 !important; }
+            .p-3:not(table), .p-3:not(.btn) > tbody > tr > td, .p-3.btn td a { padding: 12px !important; }
+            .p-6:not(table), .p-6:not(.btn) > tbody > tr > td, .p-6.btn td a { padding: 24px !important; }
+            *[class*=s-lg-] > tbody > tr > td { font-size: 0 !important; line-height: 0 !important; height: 0 !important; }
+            .s-4 > tbody > tr > td { font-size: 16px !important; line-height: 16px !important; height: 16px !important; }
+            .s-6 > tbody > tr > td { font-size: 24px !important; line-height: 24px !important; height: 24px !important; }
+            .s-10 > tbody > tr > td { font-size: 40px !important; line-height: 40px !important; height: 40px !important; }
+        }
+    </style>
+</head>
+<body class="bg-light" style="outline: 0; width: 100%; min-width: 100%; height: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; font-family: Helvetica, Arial, sans-serif; line-height: 24px; font-weight: normal; font-size: 16px; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; color: #000000; margin: 0; padding: 0; border-width: 0;" bgcolor="#f7fafc">
+<table class="bg-light body" valign="top" role="presentation" border="0" cellpadding="0" cellspacing="0" style="outline: 0; width: 100%; min-width: 100%; height: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; font-family: Helvetica, Arial, sans-serif; line-height: 24px; font-weight: normal; font-size: 16px; -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box; color: #000000; margin: 0; padding: 0; border-width: 0;" bgcolor="#f7fafc">
+    <tbody>
+    <tr>
+        <td valign="top" style="line-height: 24px; font-size: 16px; margin: 0;" align="left" bgcolor="#f7fafc">
+            <table class="container" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;">
+                <tbody>
+                <tr>
+                    <td align="center" style="line-height: 24px; font-size: 16px; margin: 0; padding: 0 16px;">
+                        <table align="center" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%; max-width: 600px; margin: 0 auto;">
+                            <tbody>
+                            <tr>
+                                <td style="line-height: 24px; font-size: 16px; margin: 0;" align="left">
+                                    <table class="s-10 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
+                                        <tbody>
+                                        <tr>
+                                            <td style="line-height: 40px; font-size: 40px; width: 100%; height: 40px; margin: 0;" align="left" width="100%" height="40">
+                                                &#160;
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    <table class="ax-center" role="presentation" align="center" border="0" cellpadding="0" cellspacing="0" style="margin: 0 auto;">
+                                        <tbody>
+                                        <tr>
+                                            <td style="line-height: 24px; font-size: 16px; margin: 0;" align="left">
+                                                <img class="w-24" src="https://i.imgur.com/OpIQgSi.png" style="height: auto; line-height: 100%; outline: none; text-decoration: none; display: block; border-style: none; border-width: 0;" width="319" height="89">
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    <table class="s-10 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
+                                        <tbody>
+                                        <tr>
+                                            <td style="line-height: 40px; font-size: 40px; width: 100%; height: 40px; margin: 0;" align="left" width="100%" height="40">
+                                                &#160;
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    <table class="card p-6 p-lg-10 space-y-4" role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-radius: 6px; border-collapse: separate !important; width: 100%; overflow: hidden; border: 1px solid #e2e8f0;" bgcolor="#ffffff">
+                                        <tbody>
+                                        <tr>
+                                            <td style="line-height: 24px; font-size: 16px; width: 100%; margin: 0; padding: 40px;" align="left" bgcolor="#ffffff">
+                                                <h1 class="h3 fw-700" style="padding-top: 0; padding-bottom: 0; font-weight: 700 !important; vertical-align: baseline; font-size: 28px; line-height: 33.6px; margin: 0;" align="left">
+                                                    Server Installed
+                                                </h1>
+                                                <table class="s-4 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td style="line-height: 16px; font-size: 16px; width: 100%; height: 16px; margin: 0;" align="left" width="100%" height="16">
+                                                            &#160;
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                                <p class="" style="line-height: 24px; font-size: 16px; width: 100%; margin: 0;" align="left">
+                                                    Your server ({{ $server->name }}) has finished installing and is now ready for you to use.
+                                                </p>
+                                                <p>
+                                                    Server Name: {{ $server->name }}
+                                                </p>
+                                                <table class="s-4 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td style="line-height: 16px; font-size: 16px; width: 100%; height: 16px; margin: 0;" align="left" width="100%" height="16">
+                                                            &#160;
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                                <table role="presentation" border="0" cellpadding="0" cellspacing="0">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td style="line-height: 24px; font-size: 16px; margin: 0;" align="left">
+                                                            <table class="btn" role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate !important;">
+                                                                <tbody>
+                                                                <tr>
+                                                                    <td style="border-radius: 6px; background-color: #3490dc;" bgcolor="#3490dc">
+                                                                        <a href="https://terminal.hostingremade.com" style="background-color: #3490dc; border: 1px solid #3490dc; border-radius: 6px; color: #ffffff; display: inline-block; font-size: 16px; font-weight: 700; line-height: 16px; padding: 16px 24px; text-decoration: none;" target="_blank">
+                                                                            Login to Terminal &rarr;
+                                                                        </a>
+                                                                    </td>
+                                                                </tr>
+                                                                </tbody>
+                                                            </table>
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                                <table class="s-6 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td style="line-height: 24px; font-size: 24px; width: 100%; height: 24px; margin: 0;" align="left" width="100%" height="24">
+                                                            &#160;
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                                <p class="" style="line-height: 24px; font-size: 16px; width: 100%; margin: 0;" align="left">
+                                                    If you did not purchase this service, please contact us at <a href="mailto:support@hostingremade.net" class="hover-underline" style="text-decoration: underline;">support@hostingremade.net</a>.
+                                                </p>
+                                                <table class="s-4 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td style="line-height: 16px; font-size: 16px; width: 100%; height: 16px; margin: 0;" align="left" width="100%" height="16">
+                                                            &#160;
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                                <p class="" style="line-height: 24px; font-size: 16px; width: 100%; margin: 0;" align="left">
+                                                    Thank you,
+                                                    <br>The HostingRemade Team
+                                                </p>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    <table class="s-10 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
+                                        <tbody>
+                                        <tr>
+                                            <td style="line-height: 40px; font-size: 40px; width: 100%; height: 40px; margin: 0;" align="left" width="100%" height="40">
+                                                &#160;
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    <table class="card p-6 p-lg-10 space-y-4" role="presentation" border="0" cellpadding="0" cellspacing="0" style="border-radius: 6px; border-collapse: separate !important; width: 100%; overflow: hidden; border: 1px solid #e2e8f0;" bgcolor="#ffffff">
+                                        <tbody>
+                                        <tr>
+                                            <td style="line-height: 24px; font-size: 12px; width: 100%; margin: 0;" align="left" bgcolor="#ffffff">
+                                                <p class="text-center" style="line-height: 24px; font-size: 12px; width: 100%; margin: 0;" align="center">
+                                                    Sent from HostingRemade Limited - Please DO NOT reply to this email
+                                                </p>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                    <table class="s-10 w-full" role="presentation" border="0" cellpadding="0" cellspacing="0" style="width: 100%;" width="100%">
+                                        <tbody>
+                                        <tr>
+                                            <td style="line-height: 40px; font-size: 40px; width: 100%; height: 40px; margin: 0;" align="left" width="100%" height="40">
+                                                &#160;
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+    </tbody>
+</table>
+</body>
+</html>
